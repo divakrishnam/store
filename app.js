c14111014fc9a606e8ed5e6bb59de5b2b6970d10
Diva Krishna <divakrishnam@gmail.com>
2021-11-20 20:59:08 +0800

    initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+const app = express();
+
+const connectDB = require('./db/connect');
+const productsRouter = require('./routes/products');
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.use('/api/v1/products', productsRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 5000;
+
+(async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+})();
